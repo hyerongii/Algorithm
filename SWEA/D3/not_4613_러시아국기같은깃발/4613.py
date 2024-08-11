494d5a23712e03abc6888747821a4e8d2625162d
hyerongii <heyrong22@gmail.com>
2024-08-11 22:22:43 +0900

    update_0811

---
+++ SWEA/D3/not_4613_러시아국기같은깃발/4613.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [list(input()) for _ in range(N)]
+    #
